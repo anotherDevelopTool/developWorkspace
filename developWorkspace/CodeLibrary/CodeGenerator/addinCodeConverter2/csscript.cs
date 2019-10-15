b579040b533f652068629c31d906a78cd9a0876d
敬江 徐 <xujingjiang@outlook.com>
2019-10-15 10:21:09 +0000

    integrate with winmerge

--- developWorkspace/CodeLibrary/CodeGenerator/addinCodeConverter2/csscript.cs
+++ developWorkspace/CodeLibrary/CodeGenerator/addinCodeConverter2/csscript.cs
@@ -57,6 +57,32 @@ public class Script
         string currentExt = "ConvertRule";
 
         [MethodMeta(Name = "变换", Date = "2009-07-20", Description = "变换", LargeIcon = "convert")]
+        public void EventHandler1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                VelocityEngine vltEngine = new VelocityEngine();
+                vltEngine.Init();
+
+                //CSV format with tab delimiter
+                var dic = DevelopWorkspace.Base.Codec.CodeSupport.getSchemaDictionary(reogrid);
+                DevelopWorkspace.Base.Logger.WriteLine("----------------schema information begin-----------------------------", Level.DEBUG);
+                DevelopWorkspace.Base.Logger.WriteLine(DevelopWorkspace.Base.Dump.ToDump(dic), Level.DEBUG);
+                DevelopWorkspace.Base.Logger.WriteLine("----------------schema information end-------------------------------", Level.DEBUG);
+
+                VelocityContext vltContext = new VelocityContext();
+                vltContext.Put("root", dic);
+
+                StringWriter vltWriter = new StringWriter();
+                vltEngine.Evaluate(vltContext, vltWriter, "", convertRule.Text);
+                DevelopWorkspace.Base.Logger.WriteLine(vltWriter.GetStringBuilder().ToString());
+			}
+            catch (Exception ex)
+            {
+                DevelopWorkspace.Base.Logger.WriteLine(ex.ToString());
+            }
+        }
+        [MethodMeta(Name = "文件做成", Date = "2009-07-20", Description = "文件做成", LargeIcon = "convert")]
         public void EventHandler2(object sender, RoutedEventArgs e)
         {
             try
@@ -69,28 +95,93 @@ public class Script
                 DevelopWorkspace.Base.Logger.WriteLine("----------------schema information begin-----------------------------", Level.DEBUG);
                 DevelopWorkspace.Base.Logger.WriteLine(DevelopWorkspace.Base.Dump.ToDump(dic), Level.DEBUG);
                 DevelopWorkspace.Base.Logger.WriteLine("----------------schema information end-------------------------------", Level.DEBUG);
-                Dictionary<string, object> Setting = dic["Setting"] as Dictionary<string, object>;
-                DevelopWorkspace.Base.Logger.WriteLine(Setting["CodePath"].ToString(), Level.DEBUG);
-                
-                //System.Diagnostics.Process.Start(@"C:\My Documents\My Pictures");
-                //string projectPath = System.IO.Path.Combine(System.Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "rba-bo-api");
-                //Directory.CreateDirectory(@"C:\workspace\csharp\WPF Extended DataGrid 2015\1\2");
-                //System.IO.File.WriteAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "addins", classAttribute.Name + "." + ext), strXaml);
+				
+				Dictionary<string, object> Setting = dic["Setting"] as Dictionary<string, object>;
+                string project = Setting["Project"].ToString();
+                string codeTempBasePath = Setting["CodeTempBasePath"].ToString();
+                string codeTempPath = Setting["CodeTempPath"].ToString();
+                string resourceTempPath = Setting["ResourceTempPath"].ToString();
+                string codeBasePath = Setting["CodeBasePath"].ToString();
+                string codePath = Setting["CodePath"].ToString();
+                string resourcePath = Setting["ResourcePath"].ToString();
+				string datasource = "core";
+				string classname = "classname";
 
-                //System.Diagnostics.Process.Start("EXPLORER.EXE", @"/e,""C:\My Documents\My Pictures""");
+				DevelopWorkspace.Base.Logger.WriteLine(codeTempPath);
+				DevelopWorkspace.Base.Logger.WriteLine(resourceTempPath);
 
-                //Dim args As String = String.Empty
-                //args = " /r /u /wl /wr /dl ""{0}"" /dr ""{1}"" ""{2}"" ""{3}"" "
-                //args = String.Format(args, "VSS(VisualSourceSafe上のファイル)", "Local(編集中のファイル）", _leftPath, _rightPath)
-                //Dim p As System.Diagnostics.Process = System.Diagnostics.Process.Start(WIN_MERGE_PATH, args)
+				Dictionary<string, object> tableInfo = dic["TableInfo"] as Dictionary<string, object>;
+                if(tableInfo["DataSource"].ToString().EndsWith("core")){
+					datasource = "core";
+				}
+				else if(tableInfo["DataSource"].ToString().EndsWith("front")){
+					datasource = "front";
+				}
+				else if(tableInfo["DataSource"].ToString().EndsWith("ics")){
+					datasource = "ics";
+				}
+				classname = tableInfo["ClassName"].ToString();
+				DevelopWorkspace.Base.Logger.WriteLine(classname);
+				
+				string entityPath = System.IO.Path.Combine(codeTempPath,"db",datasource,"entity");
+				Directory.CreateDirectory(entityPath);
+				entityPath = System.IO.Path.Combine(entityPath,classname + "Entity.java");
+				string sqlPath = System.IO.Path.Combine(resourceTempPath,"db",datasource,"dao",classname + "Dao");
+				Directory.CreateDirectory(sqlPath);
+				string crudSqlPath = System.IO.Path.Combine(sqlPath,classname + "create.sql");
+				
+				string modelPath = System.IO.Path.Combine(codeTempPath,"model","biz");
+				if(tableInfo.ContainsKey("functionId")){
+					modelPath = System.IO.Path.Combine(modelPath,tableInfo["functionId"].ToString());
+				}
 
+				
                 VelocityContext vltContext = new VelocityContext();
                 vltContext.Put("root", dic);
 
                 StringWriter vltWriter = new StringWriter();
-                vltEngine.Evaluate(vltContext, vltWriter, "", convertRule.Text);
-                DevelopWorkspace.Base.Logger.WriteLine(vltWriter.GetStringBuilder().ToString());
-            }
+				// entity
+                vltEngine.Evaluate(vltContext, vltWriter, "", getResByExt("2.ConvertRule"));
+                System.IO.File.WriteAllText(entityPath,vltWriter.GetStringBuilder().ToString());
+				
+				// model
+				vltWriter = new StringWriter();
+                vltEngine.Evaluate(vltContext, vltWriter, "", getResByExt("3.ConvertRule"));
+				modelPath = System.IO.Path.Combine(modelPath,"req",classname + "ReqModel.java");
+                System.IO.File.WriteAllText(modelPath,vltWriter.GetStringBuilder().ToString());
+
+				// SelectSQL
+				vltWriter = new StringWriter();
+                vltEngine.Evaluate(vltContext, vltWriter, "", getResByExt("5.ConvertRule"));
+				crudSqlPath = System.IO.Path.Combine(sqlPath,"selectByPrimaryKey.sql");
+                System.IO.File.WriteAllText(crudSqlPath,vltWriter.GetStringBuilder().ToString());
+
+				// InsertSQL
+				vltWriter = new StringWriter();
+                vltEngine.Evaluate(vltContext, vltWriter, "", getResByExt("6.ConvertRule"));
+				crudSqlPath = System.IO.Path.Combine(sqlPath,"insert.sql");
+                System.IO.File.WriteAllText(crudSqlPath,vltWriter.GetStringBuilder().ToString());
+
+				// UpdateSQL
+				vltWriter = new StringWriter();
+                vltEngine.Evaluate(vltContext, vltWriter, "", getResByExt("7.ConvertRule"));
+				crudSqlPath = System.IO.Path.Combine(sqlPath,"updateByPrimaryKey.sql");
+                System.IO.File.WriteAllText(crudSqlPath,vltWriter.GetStringBuilder().ToString());
+
+				// DeleteSQL
+				vltWriter = new StringWriter();
+                vltEngine.Evaluate(vltContext, vltWriter, "", getResByExt("8.ConvertRule"));
+				crudSqlPath = System.IO.Path.Combine(sqlPath,"deleteByPrimaryKey.sql");
+                System.IO.File.WriteAllText(crudSqlPath,vltWriter.GetStringBuilder().ToString());
+				
+				string WIN_MERGE_PATH = @"C:\Program Files (x86)\WinMerge\WinMergeU.exe";
+				string args = "";
+				args = @" /r /u /wl /wr /dl ""{0}"" /dr ""{1}"" ""{2}"" ""{3}"" ";
+				args = String.Format(args, "generated code", "git", codeTempBasePath, codeBasePath);
+				System.Diagnostics.Process.Start(WIN_MERGE_PATH, args);
+
+
+			}
             catch (Exception ex)
             {
                 DevelopWorkspace.Base.Logger.WriteLine(ex.ToString());
@@ -103,14 +194,6 @@ public class Script
             reogrid.Save(getResPathByExt("xlsx"));
             saveResByExt(convertRule.Text, currentExt);
 
-            DevelopWorkspace.Base.Logger.WriteLine("Process called");
-        }
-        [MethodMeta(Name = "elastic mapping", Date = "2009-07-20", Description = "read", LargeIcon = "template")]
-        public void EventHandler5(object sender, RoutedEventArgs e)
-        {
-            reogrid.Load(getResPathByExt("xlsx"), unvell.ReoGrid.IO.FileFormat.Excel2007);
-            currentExt = "ConvertRule";
-            convertRule.Text = getResByExt("ConvertRule");
             DevelopWorkspace.Base.Logger.WriteLine("Process called");
         }
         [MethodMeta(Name = "entity", Date = "2009-07-20", Description = "read", LargeIcon = "template")]
@@ -129,7 +212,7 @@ public class Script
             convertRule.Text = getResByExt("3.ConvertRule");
             DevelopWorkspace.Base.Logger.WriteLine("Process called");
         }
-        [MethodMeta(Name = "builder", Date = "2009-07-20", Description = "read", LargeIcon = "template")]
+        [MethodMeta(Name = "model(client)", Date = "2009-07-20", Description = "read", LargeIcon = "template")]
         public void EventHandler8(object sender, RoutedEventArgs e)
         {
             reogrid.Load(getResPathByExt("xlsx"), unvell.ReoGrid.IO.FileFormat.Excel2007);
@@ -137,12 +220,36 @@ public class Script
             convertRule.Text = getResByExt("4.ConvertRule");
             DevelopWorkspace.Base.Logger.WriteLine("Process called");
         }
-        [MethodMeta(Name = "TableSchemeCsv", Date = "2009-07-20", Description = "read", LargeIcon = "template")]
+        [MethodMeta(Name = "SelectSQL", Date = "2009-07-20", Description = "read", LargeIcon = "template")]
         public void EventHandler9(object sender, RoutedEventArgs e)
         {
             reogrid.Load(getResPathByExt("xlsx"), unvell.ReoGrid.IO.FileFormat.Excel2007);
-            currentExt = "9.ConvertRule";
-            convertRule.Text = getResByExt("9.ConvertRule");
+            currentExt = "5.ConvertRule";
+            convertRule.Text = getResByExt("5.ConvertRule");
+            DevelopWorkspace.Base.Logger.WriteLine("Process called");
+        }
+        [MethodMeta(Name = "InsertSQL", Date = "2009-07-20", Description = "read", LargeIcon = "template")]
+        public void EventHandler10(object sender, RoutedEventArgs e)
+        {
+            reogrid.Load(getResPathByExt("xlsx"), unvell.ReoGrid.IO.FileFormat.Excel2007);
+            currentExt = "6.ConvertRule";
+            convertRule.Text = getResByExt("6.ConvertRule");
+            DevelopWorkspace.Base.Logger.WriteLine("Process called");
+        }
+        [MethodMeta(Name = "UpdateSQL", Date = "2009-07-20", Description = "read", LargeIcon = "template")]
+        public void EventHandler11(object sender, RoutedEventArgs e)
+        {
+            reogrid.Load(getResPathByExt("xlsx"), unvell.ReoGrid.IO.FileFormat.Excel2007);
+            currentExt = "7.ConvertRule";
+            convertRule.Text = getResByExt("7.ConvertRule");
+            DevelopWorkspace.Base.Logger.WriteLine("Process called");
+        }
+        [MethodMeta(Name = "DeleteSQL", Date = "2009-07-20", Description = "read", LargeIcon = "template")]
+        public void EventHandler12(object sender, RoutedEventArgs e)
+        {
+            reogrid.Load(getResPathByExt("xlsx"), unvell.ReoGrid.IO.FileFormat.Excel2007);
+            currentExt = "8.ConvertRule";
+            convertRule.Text = getResByExt("8.ConvertRule");
             DevelopWorkspace.Base.Logger.WriteLine("Process called");
         }
         public override UserControl getView(string strXaml)
