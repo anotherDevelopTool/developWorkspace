7052a71ea2fbd90e9136d68baf8ec01a930a2191
xujingjiang <xujiingjiang@outlook.com>
2020-12-05 17:37:38 +0800

    update

--- CodeLibrary/itforce/mockapi/csscript.cs
+++ CodeLibrary/itforce/mockapi/csscript.cs
@@ -4,6 +4,9 @@ using System.IO;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using System.Linq;
+using System.Data.SQLite;
+using System.Data;
+using System.Data.Common;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -32,6 +35,7 @@ using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Serialization;
 using System.Net;
+using System.Text.RegularExpressions;
 
 public class EndPointInfo : ViewModelBase
 {
@@ -80,10 +84,237 @@ public class EndPointInfo : ViewModelBase
 }
 public class Rule
 {
-    public string Pattern { get; set; }
-    public string FileName { get; set; }
+    public string ContentType { get; set; }
+    public string EndPoint { get; set; }
+    public string MatchString { get; set; }
+    public string ResponseFile { get; set; }
+    public int Likeness { get; set; }
 }
+public class Levenshtein
+{
 
+
+    ///*****************************
+    /// Compute Levenshtein distance 
+    /// Memory efficient version
+    ///*****************************
+    public int iLD(String sRow, String sCol)
+    {
+        int RowLen = sRow.Length;  // length of sRow
+        int ColLen = sCol.Length;  // length of sCol
+        int RowIdx;                // iterates through sRow
+        int ColIdx;                // iterates through sCol
+        char Row_i;                // ith character of sRow
+        char Col_j;                // jth character of sCol
+        int cost;                   // cost
+
+        /// Test string length
+        if (Math.Max(sRow.Length, sCol.Length) > Math.Pow(2, 31))
+            throw (new Exception("\nMaximum string length in Levenshtein.iLD is " + Math.Pow(2, 31) + ".\nYours is " + Math.Max(sRow.Length, sCol.Length) + "."));
+
+        // Step 1
+
+        if (RowLen == 0)
+        {
+            return ColLen;
+        }
+
+        if (ColLen == 0)
+        {
+            return RowLen;
+        }
+
+        /// Create the two vectors
+        int[] v0 = new int[RowLen + 1];
+        int[] v1 = new int[RowLen + 1];
+        int[] vTmp;
+
+
+
+        /// Step 2
+        /// Initialize the first vector
+        for (RowIdx = 1; RowIdx <= RowLen; RowIdx++)
+        {
+            v0[RowIdx] = RowIdx;
+        }
+
+        // Step 3
+
+        /// Fore each column
+        for (ColIdx = 1; ColIdx <= ColLen; ColIdx++)
+        {
+            /// Set the 0'th element to the column number
+            v1[0] = ColIdx;
+
+            Col_j = sCol[ColIdx - 1];
+
+
+            // Step 4
+
+            /// Fore each row
+            for (RowIdx = 1; RowIdx <= RowLen; RowIdx++)
+            {
+                Row_i = sRow[RowIdx - 1];
+
+
+                // Step 5
+
+                if (Row_i == Col_j)
+                {
+                    cost = 0;
+                }
+                else
+                {
+                    cost = 1;
+                }
+
+                // Step 6
+
+                /// Find minimum
+                int m_min = v0[RowIdx] + 1;
+                int b = v1[RowIdx - 1] + 1;
+                int c = v0[RowIdx - 1] + cost;
+
+                if (b < m_min)
+                {
+                    m_min = b;
+                }
+                if (c < m_min)
+                {
+                    m_min = c;
+                }
+
+                v1[RowIdx] = m_min;
+            }
+
+            /// Swap the vectors
+            vTmp = v0;
+            v0 = v1;
+            v1 = vTmp;
+
+        }
+
+
+        // Step 7
+
+        /// Value between 0 - 100
+        /// 0==perfect match 100==totaly different
+        /// 
+        /// The vectors where swaped one last time at the end of the last loop,
+        /// that is why the result is now in v0 rather than in v1
+        System.Console.WriteLine("iDist=" + v0[RowLen]);
+        int max = System.Math.Max(RowLen, ColLen);
+        return ((100 * v0[RowLen]) / max);
+    }
+
+
+
+
+
+    ///*****************************
+    /// Compute the min
+    ///*****************************
+
+    private int Minimum(int a, int b, int c)
+    {
+        int mi = a;
+
+        if (b < mi)
+        {
+            mi = b;
+        }
+        if (c < mi)
+        {
+            mi = c;
+        }
+
+        return mi;
+    }
+
+    ///*****************************
+    /// Compute Levenshtein distance         
+    ///*****************************
+
+    public int LD(String sNew, String sOld)
+    {
+        int[,] matrix;              // matrix
+        int sNewLen = sNew.Length;  // length of sNew
+        int sOldLen = sOld.Length;  // length of sOld
+        int sNewIdx; // iterates through sNew
+        int sOldIdx; // iterates through sOld
+        char sNew_i; // ith character of sNew
+        char sOld_j; // jth character of sOld
+        int cost; // cost
+
+        /// Test string length
+        if (Math.Max(sNew.Length, sOld.Length) > Math.Pow(2, 31))
+            throw (new Exception("\nMaximum string length in Levenshtein.LD is " + Math.Pow(2, 31) + ".\nYours is " + Math.Max(sNew.Length, sOld.Length) + "."));
+
+        // Step 1
+
+        if (sNewLen == 0)
+        {
+            return sOldLen;
+        }
+
+        if (sOldLen == 0)
+        {
+            return sNewLen;
+        }
+
+        matrix = new int[sNewLen + 1, sOldLen + 1];
+
+        // Step 2
+
+        for (sNewIdx = 0; sNewIdx <= sNewLen; sNewIdx++)
+        {
+            matrix[sNewIdx, 0] = sNewIdx;
+        }
+
+        for (sOldIdx = 0; sOldIdx <= sOldLen; sOldIdx++)
+        {
+            matrix[0, sOldIdx] = sOldIdx;
+        }
+
+        // Step 3
+
+        for (sNewIdx = 1; sNewIdx <= sNewLen; sNewIdx++)
+        {
+            sNew_i = sNew[sNewIdx - 1];
+
+            // Step 4
+
+            for (sOldIdx = 1; sOldIdx <= sOldLen; sOldIdx++)
+            {
+                sOld_j = sOld[sOldIdx - 1];
+
+                // Step 5
+
+                if (sNew_i == sOld_j)
+                {
+                    cost = 0;
+                }
+                else
+                {
+                    cost = 1;
+                }
+
+                // Step 6
+
+                matrix[sNewIdx, sOldIdx] = Minimum(matrix[sNewIdx - 1, sOldIdx] + 1, matrix[sNewIdx, sOldIdx - 1] + 1, matrix[sNewIdx - 1, sOldIdx - 1] + cost);
+
+            }
+        }
+
+        // Step 7
+
+        /// Value between 0 - 100
+        /// 0==perfect match 100==totaly different
+        DevelopWorkspace.Base.Logger.WriteLine("Dist=" + matrix[sNewLen, sOldLen]);
+        int max = System.Math.Max(sNewLen, sOldLen);
+        return (100 * matrix[sNewLen, sOldLen]) / max;
+    }
+}
 public class Script
 {
     [AddinMeta(Name = "mockapi", Date = "2009-07-20", Description = "mockapi", LargeIcon = "apimock", Red = 128, Green = 145, Blue = 213)]
@@ -92,8 +323,10 @@ public class Script
         System.Windows.Controls.ListView listView;
         List<EndPointInfo> endPointInfoList = new List<EndPointInfo>();
         HttpListener _listener = new HttpListener();
+        List<Rule> rules = new List<Rule>();
+        Levenshtein l = new Levenshtein();
 
-        [MethodMeta(Name = "ON", Date = "2009-07-20", Description = "ON", LargeIcon = "apiconect")]
+        [MethodMeta(Name = "ON", Date = "2009-07-20", Description = "turn Mock Api Service ON", LargeIcon = "apiconect")]
         public void EventHandler1(object sender, RoutedEventArgs e)
         {
             try
@@ -108,10 +341,10 @@ public class Script
             }
             catch (Exception ex)
             {
-                DevelopWorkspace.Base.Logger.WriteLine(ex.ToString());
+                DevelopWorkspace.Base.Logger.WriteLine(ex.ToString(), Level.ERROR);
             }
         }
-        [MethodMeta(Name = "OFF", Date = "2009-07-20", Description = "OFF", LargeIcon = "disconect")]
+        [MethodMeta(Name = "OFF", Date = "2009-07-20", Description = "turn Mock Api Service OFF", LargeIcon = "disconect")]
         public void EventHandler2(object sender, RoutedEventArgs e)
         {
             try
@@ -120,9 +353,7 @@ public class Script
             }
             catch (Exception ex)
             {
-                DevelopWorkspace.Base.Logger.WriteLine(ex.ToString());
             }
-
         }
 
         public void GetContextCallback(IAsyncResult result)
@@ -136,59 +367,66 @@ public class Script
                 System.Text.StringBuilder sb = new System.Text.StringBuilder();
 
                 sb.Append("");
-                sb.Append(string.Format("HttpMethod: {0}", request.HttpMethod));
+                sb.Append(string.Format("HttpMethod: {0}\n", request.HttpMethod));
 
-                sb.Append(string.Format("Uri:        {0}", request.Url.AbsoluteUri));
+                sb.Append(string.Format("Uri:        {0}\n", request.Url.AbsoluteUri));
 
-                sb.Append(string.Format("LocalPath:  {0}", request.Url.LocalPath));
+                sb.Append(string.Format("LocalPath:  {0}\n", request.Url.LocalPath));
                 foreach (string key in request.QueryString.Keys)
                 {
-                    sb.Append(string.Format("Query:      {0} = {1}", key, request.QueryString[key]));
+                    sb.Append(string.Format("Query:      {0} = {1}\n", key, request.QueryString[key]));
                 }
                 sb.Append("");
-
                 string responseString = sb.ToString();
-
                 DevelopWorkspace.Base.Logger.WriteLine(responseString);
 
 
-                response.StatusCode = (int)HttpStatusCode.OK;
-                response.ContentType = "application/json";
-                response.ContentEncoding = Encoding.UTF8;
-
-
-                responseString = "";
-                if (request.Url.LocalPath.Equals("/rba-backend-item-api/items/findproductsinfo"))
-                {
-                    responseString = System.IO.File.ReadAllText(@"C:\xujingjiang\tools\developWorkspace1.5\CodeLibrary\itforce\mockapi\findProductsInfoRes_WhenSuccess.json", Encoding.UTF8);
-                }
-
-
-                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
-                response.ContentLength64 = buffer.Length;
-
-                string documentContents;
+                string requestString;
                 using (Stream receiveStream = request.InputStream)
                 {
                     using (StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8))
                     {
-                        documentContents = readStream.ReadToEnd();
+                        requestString = readStream.ReadToEnd();
                     }
                 }
 
-                DevelopWorkspace.Base.Logger.WriteLine(documentContents);
+                DevelopWorkspace.Base.Logger.WriteLine(requestString);
 
-
-                //request.Dump();
-
-
+                List<Rule> findedRules = rules.Where(rule => (rule.EndPoint.Equals(request.Url.LocalPath))).ToList();
+                findedRules.ForEach(rule => { rule.Likeness = l.LD(requestString, rule.MatchString); });
+                Rule findedRule = findedRules.Where(rule => rule.Likeness == findedRules.Min(r => r.Likeness)).FirstOrDefault();
                 using (System.IO.Stream outputStream = response.OutputStream)
                 {
-                    outputStream.Write(buffer, 0, buffer.Length);
+
+                    if (findedRule == null)
+                    {
+                        DevelopWorkspace.Base.Logger.WriteLine("please set suitable response file", Level.WARNING);
+                    }
+                    else
+                    {
+                        findedRule.Dump();
+                        response.StatusCode = (int)HttpStatusCode.OK;
+                        response.ContentType = findedRule.ContentType;
+                        response.ContentEncoding = Encoding.UTF8;
+                        
+                        string responseFilePath = findedRule.ResponseFile;
+                        if (FileExist(ref responseFilePath))
+                        {
+                            responseString = System.IO.File.ReadAllText(responseFilePath, Encoding.UTF8);
+
+                            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+
+
+                            response.ContentLength64 = buffer.Length;
+                            outputStream.Write(buffer, 0, buffer.Length);
+                        }
+                        else
+                        {
+                            DevelopWorkspace.Base.Logger.WriteLine(responseFilePath + " does not exists,please confirm setting", Level.WARNING);
+                        }
+                    }
+                    outputStream.Close();
                 }
-
-
-
 
                 _listener.BeginGetContext(new AsyncCallback(GetContextCallback), null);
             }
@@ -196,6 +434,26 @@ public class Script
             {
                 DevelopWorkspace.Base.Logger.WriteLine(ex.ToString());
             }
+        }
+
+        public bool FileExist(ref string filename)
+        {
+            if (Regex.IsMatch(filename, "^[a-z]:", RegexOptions.IgnoreCase))
+            {
+            }
+            else
+            {
+                filename = System.IO.Path.Combine(DevelopWorkspace.Main.StartupSetting.instance.homeDir, filename);
+            }
+            if (File.Exists(filename))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
         }
 
         public override UserControl getView(string strXaml)
@@ -212,6 +470,32 @@ public class Script
             };
             listView.DataContext = endPointInfoList;
             listView.SelectedIndex = 0;
+
+            try
+            {
+                using (SQLiteConnection sqLiteConnection = new SQLiteConnection(@"Data Source = addins.db; Pooling = true; FailIfMissing = false"))
+                {
+                    sqLiteConnection.Open();
+                    SQLiteCommand sqLiteCommand = new SQLiteCommand(sqLiteConnection)
+                    {
+                        CommandText = "SELECT * FROM EndPointRules"
+                    };
+
+                    using (DbDataReader rdr = sqLiteCommand.ExecuteReader())
+                    {
+                        while (rdr.Read())
+                        {
+                            rules.Add(new Rule { ContentType = rdr["ContentType"].ToString(), EndPoint = rdr["EndPoint"].ToString(), MatchString = rdr["MatchString"].ToString(), ResponseFile = rdr["ResponseFile"].ToString(), Likeness = 0 });
+                        }
+                    }
+                    sqLiteConnection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                DevelopWorkspace.Base.Logger.WriteLine(ex.ToString(), Level.ERROR);
+
+            }
 
             return view;
         }
